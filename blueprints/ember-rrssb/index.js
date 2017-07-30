9d108596fd5b5dfc4f4676c92c78ee4230cbc5eb
saurs saurav <saurssaurav33@gmail.com>
2017-07-30 13:24:04 +0545

    fixes in blueprint

--- blueprints/ember-rrssb/index.js
+++ blueprints/ember-rrssb/index.js
@@ -1,12 +1,10 @@
 /* eslint-env node */
 module.exports = {
-  description: ''
-  included: function(app) {
-    this._super.included(app);
-
-    app.import('bower_components/rrssb/css/rrssb.css');
-    app.import('bower_components/rrssb/js/rrssb.js');
+  description: 'blueprintone',
+  normalizeEntityName: function(){
+    return "ember-rrssb";
   },
+
   afterInstall(options) {
     // Perform extra work here.
     return this.addBowerPackageToProject('rrssb');
--- index.js
+++ index.js
@@ -2,5 +2,10 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-rrssb'
+  name: 'ember-rrssb',
+  included: function(app) {
+    this._super.included(app);
+    app.import('bower_components/rrssb/css/rrssb.css');
+    app.import('bower_components/rrssb/js/rrssb.js');
+  }
 };
