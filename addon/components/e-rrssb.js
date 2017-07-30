dcb7dc851de36ac2d2f90cd49cc14f835e76a265
saurs saurav <saurssaurav33@gmail.com>
2017-07-30 14:01:22 +0545

    added more social share button

--- addon/components/e-rrssb.js
+++ addon/components/e-rrssb.js
@@ -3,10 +3,26 @@ import layout from '../templates/components/e-rrssb';
 
 export default Ember.Component.extend({
   layout,
-  facebookurl: '',
-  twitterurl:'',
+  shareUrl:'',
+  emailSubject:'',
+
+  email:false,
+  facebook:false,
+  tumblr:false,
+  linkedin:false,
+  xing:false,
+  twitter:false,
+  reddit:false,
+  vk:false,
+  googleplus:false,
+  pocket:false,
+  pinterest:false,
+
   init() {
     this._super(...arguments);
+    if(this.get('shareUrl') === ''){
+      this.set('shareUrl',window.location.href);
+    }
   },
   didInsertElement(){
     window.rrssbInit();
